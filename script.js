fd84ca6d2102b4d9256316e16f965b936e18b6b8
Felipe Alvarez <n.felipealvarez01@gmail.com>
2022-07-24 21:41:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingrese su nombre")
+
+alert('Bienvenido/a' + ' ' + nombre);
+
+let numeroUsuario = parseInt(prompt('Ingrese un número menor a 10, para que se repita "Hola" por esa cantidad elegida.'));
+
+for (let i = 0; i < numeroUsuario; i++) {
+    alert("Hola");
+}
